12cf8aa5bb0ce6ef22202f5a6aa61620544701be
qawse6 <qawse6@daum.net>
2023-11-14 16:37:16 +0900

    (SCRUM-5) [Add] 투명화되는 적 추가

---
+++ src/entity/EnemyShipE.java
@@ -0,0 +1,44 @@
+package entity;
+
+import engine.Cooldown;
+import engine.DrawManager;
+import engine.GameState;
+
+import java.awt.*;
+import java.util.Set;
+
+public class EnemyShipE extends EnemyShip {
+    /** HP의 배율 */
+    private final double HPPOWER = .1;
+    /** 총알의 속도 배율 */
+    private final double BULLETSPEEDPOWER = 2;
+    /** 슈팅 쿨다운 배율 */
+    private final double BULLETCOOLDOWN = 0.5;
+    /** 제거시 올라가는 점수 */
+    private final int POINT = 20;
+    public EnemyShipE(final int positionX, final int positionY,
+                      final DrawManager.SpriteType spriteType, final GameState gameState) {
+        super(positionX, positionY, spriteType, gameState);
+        super.HP = (int)(super.HP * HPPOWER);
+        super.pointValue = POINT;
+    }
+
+    public final void update() {
+        if (this.animationCooldown.checkFinished()) {
+            this.animationCooldown.reset();
+            if (spriteType == DrawManager.SpriteType.EnemyShipC1) {
+                spriteType = DrawManager.SpriteType.EnemyShipC2;
+                this.setColor(Color.BLACK);
+            }
+            else {
+                spriteType = DrawManager.SpriteType.EnemyShipC1;
+                this.setColor(Color.WHITE);
+            }
+        }
+    }
+    public final void shoot(final Set<Bullet> bullets, Cooldown shootingCooldown) {
+        bullets.add(BulletPool.getBullet(positionX
+                + width / 2, positionY, (int)(super.BULLET_SPEED * BULLETSPEEDPOWER),0));
+        shootingCooldown.timedown(BULLETCOOLDOWN);
+    }
+}
